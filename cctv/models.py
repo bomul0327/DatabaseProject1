2675a77184c3c5fba275e416e1e6f6aa2fc0bef7
hyosei <jjung1995@yonsei.ac.kr>
2017-12-02 18:42:33 +0900

    Update models.py
    3차 완성본
    (__str__ 추가, 이름 확인 가능)

--- cctv/models.py
+++ cctv/models.py
@@ -6,6 +6,8 @@ class Manager(models.Model):
     pw = models.CharField(max_length=20, null=False)
     pos = models.CharField(max_length=20)
     phonenum = models.CharField(max_length=13)
+    def __str__(self):
+        return self.id
 
 class Shoot_space(models.Model):
     id = models.CharField(max_length=20, primary_key=True, null=False)
@@ -13,6 +15,8 @@ class Shoot_space(models.Model):
     building_name = models.CharField(max_length=20)
     flr = models.CharField(max_length=5)
     location = models.CharField(max_length=20)
+    def __str__(self):
+        return self.id
 
 class CCTV(models.Model):
     id = models.CharField(max_length=20, primary_key=True, null=False)
@@ -20,6 +24,8 @@ class CCTV(models.Model):
     install_date = models.DateTimeField('date installed')
     manager = models.ForeignKey(Manager, on_delete=models.CASCADE, null=False)
     shoots = models.ManyToManyField(Shoot_space, through='Shoot', through_fields=('CCTV_id', 'Shoot_space_id'))
+    def __str__(self):
+        return self.id
 
 class Shoot (models.Model):
     CCTV_id = models.ForeignKey(CCTV, on_delete=models.CASCADE, null=False)
@@ -31,6 +37,8 @@ class Files(models.Model):
     end_time = models.DateTimeField('end time')
     CCTV_id = models.ForeignKey(CCTV, on_delete=models.CASCADE, null=False)
     Shoot_space_id = models.ForeignKey(Shoot_space, on_delete=models.CASCADE, null=False)
+    def __str__(self):
+        return self.file_name
 
 class Neighborhood(models.Model):
     id = models.CharField(max_length=20, primary_key=True, null=False)
@@ -38,10 +46,14 @@ class Neighborhood(models.Model):
     location = models.CharField(max_length=30)
     space_a = models.ForeignKey(Shoot_space, related_name='A', on_delete=models.CASCADE, null=False)
     space_b = models.ForeignKey(Shoot_space, related_name='B', on_delete=models.CASCADE, null=False)
+    def __str__(self):
+        return self.id
 
 class Sequence(models.Model):
     id = models.CharField(max_length=20, primary_key=True, null=False)
     connects = models.ManyToManyField(Neighborhood)
+    def __str__(self):
+        return self.id
 
 class Statistics(models.Model):
     file_name =  models.OneToOneField(Files)
@@ -50,6 +62,8 @@ class Statistics(models.Model):
     obj_num = models.IntegerField()
     avg_speed = models.DecimalField(max_digits=20, decimal_places=2)
     avg_size = models.DecimalField(max_digits=20, decimal_places=2)
+    def __str__(self):
+        return self.file_name
 
 class Record(models.Model):
     time_stamp = models.DateTimeField(null=False)
@@ -59,3 +73,5 @@ class Record(models.Model):
     obj_speed = models.IntegerField
     obj_color = models.CharField(max_length=20)
     file_name = models.ForeignKey(Files, null=False, on_delete=models.DO_NOTHING)
+    def __str__(self):
+        return self.file_name + ":" + self.time_stamp
