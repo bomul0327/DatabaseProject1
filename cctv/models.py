695a65a2dc6584dc5b0011a0f70a9e78ce2c1721
hyosei <jjung1995@yonsei.ac.kr>
2017-12-02 16:59:05 +0900

    Update models.py
    manager table

--- cctv/models.py
+++ cctv/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Manager(models.Model):
+    id = models.CharField(max_length=20, primary_key=True, null=False)
+    pw = models.CharField(max_length=20, null=False)
+    pos = models.CharField(max_length=20)
+    phonenum = models.CharField(max_length=13)
