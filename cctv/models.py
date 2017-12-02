dbcb8ede78733ab8fd3cc758c503b5def274e666
hyosei <jjung1995@yonsei.ac.kr>
2017-12-02 18:18:57 +0900

    Update models.py
    2차 완성본

--- cctv/models.py
+++ cctv/models.py
@@ -7,19 +7,19 @@ class Manager(models.Model):
     pos = models.CharField(max_length=20)
     phonenum = models.CharField(max_length=13)
 
-class CCTV(models.Model):
-    id = models.CharField(max_length=20, primary_key=True, null=False)
-    model_name = models.CharField(max_length=20, null=False)
-    install_date = models.DateTimeField('date installed')
-    manager = models.ForeignKey(Manager, on_delete=models.CASCADE, null=False)
-    shoots = models.ManyToManyField(Shoot_space, through='Shoot', through_fields=('CCTV_id', 'Shoot_space_id'))
-
 class Shoot_space(models.Model):
     id = models.CharField(max_length=20, primary_key=True, null=False)
     dong = models.CharField(max_length=20)
     building_name = models.CharField(max_length=20)
     flr = models.CharField(max_length=5)
     location = models.CharField(max_length=20)
+
+class CCTV(models.Model):
+    id = models.CharField(max_length=20, primary_key=True, null=False)
+    model_name = models.CharField(max_length=20, null=False)
+    install_date = models.DateTimeField('date installed')
+    manager = models.ForeignKey(Manager, on_delete=models.CASCADE, null=False)
+    shoots = models.ManyToManyField(Shoot_space, through='Shoot', through_fields=('CCTV_id', 'Shoot_space_id'))
 
 class Shoot (models.Model):
     CCTV_id = models.ForeignKey(CCTV, on_delete=models.CASCADE, null=False)
@@ -36,24 +36,20 @@ class Neighborhood(models.Model):
     id = models.CharField(max_length=20, primary_key=True, null=False)
     route = models.CharField(max_length=30)
     location = models.CharField(max_length=30)
-    space_a = models.ForeignKey(Shoot_space, on_delete=models.CASCADE, null=False)
-    space_b = models.ForeignKey(Shoot_space, on_delete=models.CASCADE, null=False)
+    space_a = models.ForeignKey(Shoot_space, related_name='A', on_delete=models.CASCADE, null=False)
+    space_b = models.ForeignKey(Shoot_space, related_name='B', on_delete=models.CASCADE, null=False)
 
 class Sequence(models.Model):
     id = models.CharField(max_length=20, primary_key=True, null=False)
-    connects = models.ManyToManyField(Neighborhood, through=Connection, through_fields=('Sequence_id', 'neighborhood_id'))
-
-class Connection(models.Model):
-    Sequence_id = models.ForeignKey(Sequence, on_delete=models.CASCADE)
-    neighborhood_id = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+    connects = models.ManyToManyField(Neighborhood)
 
 class Statistics(models.Model):
-    file_name =  models.ForeignKey(Files, primary_key=True, null=False, on_delete=models.DO_NOTHING)
+    file_name =  models.OneToOneField(Files)
     records = models.IntegerField()
     time_length = models.IntegerField()
     obj_num = models.IntegerField()
-    avg_speed = models.DecimalField()
-    avg_size = models.DecimalField()
+    avg_speed = models.DecimalField(max_digits=20, decimal_places=2)
+    avg_size = models.DecimalField(max_digits=20, decimal_places=2)
 
 class Record(models.Model):
     time_stamp = models.DateTimeField(null=False)
