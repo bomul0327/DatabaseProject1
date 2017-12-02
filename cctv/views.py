5a48efc47a88fb1119daa477ba93da66c71f4ac7
황보 정 <bomul0327@naver.com>
2017-12-02 18:00:49 +0900

    no message

--- cctv/views.py
+++ cctv/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+from .models import Manager
 
 # Create your views here.
+def index(request):
+    context = {
+
+    }
+    return render(request, 'cctv/index.html', context)
--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'cctv.apps.CctvConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
