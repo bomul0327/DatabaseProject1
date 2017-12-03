664b2673016bc71b5445db4cbcf41c02d3a5d50c
Your Name <you@example.com>
2017-12-03 15:00:19 +0900

    html templates created

--- cctv/urls.py
+++ cctv/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 #cctv/views.py에서 정의된 def 함수명(request)에서 함수명을 아래에 url(regular expresstion, views.함수명, name='함수명'),으로 추가하기
 urlpatterns = [
-    url(r'^$', views.list, name='list'),
+    url(r'^shoot_space_manage$', views.shoot_space_manage, name='shoot_space_manage'),
+    url(r'^file_name$', views.file_name, name='file_name'),
+    url(r'^log_read$', views.log_read, name='log_read'),
+    url(r'^my_page$', views.my_page, name='my_page'),
+    url(r'^user_manage$', views.user_manage, name='user_manage'),
+    url(r'^cctv_manage$', views.cctv_manage, name='cctv_manage'),
+    url(r'^space_manage$', views.space_manage, name='space_manage'),
 ]
--- cctv/views.py
+++ cctv/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
 #여기에 def로 정의한 함수 cctv/urls.py에도 추가하기
-def list(request):
+def shoot_space_manage(request):
     return render(request, 'cctv/shoot_space_manage.html', {})
+
+def file_manage(request):
+    return render(request, 'cctv/file_manage.html', {})
+
+def log_read(request):
+    return render(request, 'cctv/log_read.html', {})
+
+def my_page(request):
+    return render(request, 'cctv/my_page.html', {})
+
+def user_manage(request):
+    return render(request, 'cctv/user_manage.html', {})
+
+def cctv_manage(request):
+    return render(request, 'cctv/cctv_manage.html', {})
+
+def space_manage(request):
+    return render(request, 'cctv/space_manage.html', {})
 
 # Create your views here.
