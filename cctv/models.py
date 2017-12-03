c7ab2a51129a9441e6cab998846483d8b6c0ef23
Your Name <you@example.com>
2017-12-03 16:40:28 +0900

    유저 관리페이지 작성중.

--- cctv/models.py
+++ cctv/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
 class Manager(models.Model):
     id = models.CharField(max_length=20, primary_key=True, null=False)
     pw = models.CharField(max_length=20, null=False)
--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cctv' # cctv app 추가, by 학수
+    'cctv' # cctv app 추가
 ]
 
 MIDDLEWARE = [
