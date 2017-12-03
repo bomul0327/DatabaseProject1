6f987af89e3c000611f307bfd871b26dfd7a8556
Your Name <you@example.com>
2017-12-03 14:40:55 +0900

    url setting

--- cctv/urls.py
+++ cctv/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
+
+#cctv/views.py에서 정의된 def 함수명(request)에서 함수명을 아래에 url(regular expresstion, views.함수명, name='함수명'),으로 추가하기
 urlpatterns = [
     url(r'^$', views.list, name='list'),
 ]
--- cctv/views.py
+++ cctv/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
+#여기에 def로 정의한 함수 cctv/urls.py에도 추가하기
 def list(request):
-    return render(request, 'cctv/html/shoot_space_manage.html', {})
+    return render(request, 'cctv/shoot_space_manage.html', {})
 
 # Create your views here.
