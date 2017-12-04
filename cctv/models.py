ad0ccac9c08ef7bf8816440edceff1099a2eeac0
HBJ <bomul0327@naver.com>
2017-12-04 14:21:28 +0900

    Customized user Model

--- cctv/models.py
+++ cctv/models.py
@@ -1,8 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Manager(models.Model):
-    id = models.CharField(max_length=20, primary_key=True, null=False)
-    pw = models.CharField(max_length=20, null=False)
+    user = models.OneToOneField(User)
     pos = models.CharField(max_length=20)
     phonenum = models.CharField(max_length=13)
