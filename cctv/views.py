97650d6d3c5bd915f1c5e76aaf7656b6b804b1e8
Your Name <you@example.com>
2017-12-04 21:32:54 +0900

    CCTV 검색, 추가, 삭제기능을 cctv_manage.html 과 views.py에 구현

--- cctv/views.py
+++ cctv/views.py
@@ -11,23 +11,29 @@ def login(request):
     return render(request, 'cctv/login.html', {})
     #return HttpResponse('[%s] logged in successfully' % request.session['login_sess'])
 
+#여기에 def로 정의한 함수 cctv/urls.py에도 추가하기
 def logout(request):
     del request.session['login_sess']
     return HttpResponse('logged out successfully')
     #return render(request, 'cctv/login.html', {})
 
+#여기에 def로 정의한 함수 cctv/urls.py에도 추가하기
 def shoot_space_manage(request):
     return render(request, 'cctv/shoot_space_manage.html', {})
 
+#여기에 def로 정의한 함수 cctv/urls.py에도 추가하기
 def file_manage(request):
     return render(request, 'cctv/file_manage.html', {})
 
+#여기에 def로 정의한 함수 cctv/urls.py에도 추가하기
 def log_read(request):
     return render(request, 'cctv/log_read.html', {})
 
+#여기에 def로 정의한 함수 cctv/urls.py에도 추가하기
 def my_page(request):
     return render(request, 'cctv/my_page.html', {})
 
+#여기에 def로 정의한 함수 cctv/urls.py에도 추가하기
 def manager_manage(request):
     manager_list = Manager.objects.raw('SELECT id, pos, phonenum from cctv_manager WHERE id NOT IN ("admin")')
     with connection.cursor() as form:
@@ -51,11 +57,12 @@ def manager_manage(request):
 #        row = cursor.fetchone()
 #    return row
 
-
+#여기에 def로 정의한 함수 cctv/urls.py에도 추가하기
 def post_new(request):
     form = manager_manage_form()
     return render(request, 'cctv/post_edit.html', {'form': form})
 
+#여기에 def로 정의한 함수 cctv/urls.py에도 추가하기
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -70,8 +77,25 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
+#여기에 def로 정의한 함수 cctv/urls.py에도 추가하기
 def cctv_manage(request):
-    return render(request, 'cctv/cctv_manage.html', {})
+    cctv_list = CCTV.objects.raw('SELECT id, model_name, install_date, manager_id FROM cctv_cctv')
+    if request.method == "POST":
+        model_name = request.POST['model_name'] # 여기부터
+        if model_name == "":                    # 공백이 입력된 경우 전체 값을 검색함
+            model_name = "%"                    # 여기까지
+        manager_id = request.POST['manager_id']
+        if manager_id == "":
+            manager_id = "%"
+        cctv_list = CCTV.objects.raw('SELECT id, model_name, install_date, manager_id FROM cctv_cctv WHERE model_name like %s AND manager_id like %s', [model_name, manager_id])
+    with connection.cursor() as form:
+        form = connection.cursor()
+        if request.method == "POST" and request.POST['mode'] =="insert" :
+            form.execute("INSERT INTO cctv_cctv ('id', 'model_name', 'manager_id', 'install_date') VALUES(%s, %s, %s, %s)", [request.POST['insert_id'], request.POST['model_name'], request.POST['manager_id'], request.POST['install_date']] )
+        elif request.method == "POST" and request.POST['mode'] =="delete" :
+            form.execute('DELETE FROM cctv_cctv WHERE id = %s', [request.POST['delete_id']])
+    return render(request, 'cctv/cctv_manage.html', {'cctv_list' : cctv_list, 'form': form})
 
+#여기에 def로 정의한 함수 cctv/urls.py에도 추가하기
 def space_manage(request):
     return render(request, 'cctv/space_manage.html', {})
